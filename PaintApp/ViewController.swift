c68eb76e047f070a92346187fafdaf56e5fb2980
maehara08 <aso.riiiku08@gmail.com>
2017-07-01 00:15:59 +0900

    Initial Commit

---
+++ PaintApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PaintApp
+//
+//  Created by 前原理来 on 2017/07/01.
+//  Copyright © 2017年 前原理来. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
